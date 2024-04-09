6ec3b52fb21355a55189be3de857506bbc5a55ba
junkinatsui <dptgagtma0573@gmail.com>
2024-04-09 18:41:56 +0900

    js修正

--- main.js
+++ main.js
@@ -1,45 +1,69 @@
-const timer = document.getElementById('timer')
-const start = document.getElementById('start')
-const stop = document.getElementById('stop')
-const reset = document.getElementById('reset')
 
-let hours = 0;
-let minutes = 0;
-let seconds = 0;
-let milliseconds = 0;
 
-let stopWatch = () => {
-    seconds++;
-    if (seconds % 60 === 0) {
-        minutes++;
-        seconds = 0;
+
+let ms = 0;
+let sec = 0;
+let min = 0;
+let hour = 0;
+  
+let timeoutID;
+
+$(document).ready(function(){
+    $("#start").click(function() {
+      ms = 0;
+      sec = 0;
+      min = 0;
+      hour = 0;
+      $("#timer").html('0:0:0:0');
+      timeoutID = setInterval(countup, 100);
+  
+      $(this).prop('disabled', true);
+      $("#stop,#reset").prop('disabled', false);
+    });
+  
+    $("#stop").click(function() {
+      clearInterval(timeoutID);
+  
+      $(this).prop('disabled', true);
+      $("#start,#reset").prop('disabled', false);
+    });
+  
+    $("#reset").click(function() {
+      ms = 0;
+      sec = 0;
+      min = 0;
+      hour = 0;
+      $("#timer").html('0:0:0:0');
+      clearInterval(timeoutID);
+  
+      $("#stop,#reset").prop('disabled', true);
+      $("#start").prop('disabled', false);
+    });
+  
+
+    function countup() {
+       ms += 1;
+
+      if (ms > 9) {
+        ms = 0;
+        sec += 1;
+      }
+  
+      if (sec > 59) {
+        sec = 0;
+        min += 1;
+      }
+  
+      if (min > 59) {
+        min = 0;
+        hour += 1;
+      }
+
+      ms_number = ('0' + ms).slice(-1);
+      sec_number = ('00' + sec).slice(-2);
+      min_number = ('00' + min).slice(-2);
+      hour_number = ('00' + hour).slice(-1);
+  
+      $("#timer").html(hour_number + ':' +  min_number + ':' + sec_number + ':' + ms_number);
     }
-    timer.innerHTML = ('0' + hours).slice(-2) + ':' + ('0' + minutes).slice(-2) + ':' + ('0' + seconds).slice(-2) + ':' + ('0' + milliseconds).slice(-2);
-}
-
-let interval;
-
-start.addEventListener('click' , () => {
-    interval = setinterval(stopWatch, 1000);
-    start.disabled = true;
-    stop.disabled = false;
-    reset.disabled = true;
-})
-
-stop.addEventListener('ckick' , () => {
-    clearInterval(interval);
-    start.disabled = false;
-    stop.disabled = true;
-    reset.disabled = false;
-})
-
-reset.addEventListener('click' , () => {
-    timer.innerHTML = '0:0:0:0';
-    hours = 0;
-    minutes = 0;
-    seconds = 0;
-    milliseconds = 0;
-    start.disabled = false;
-    stop.disabled = true;
-    reset.disabled = true;
-})
+});
